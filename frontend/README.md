41f5111c045f7c2317ccb1c49c6aadecb6d6e09f
= <=>
2025-06-08 20:31:47 +0545

    ready set go

--- frontend/README.md
+++
@@ -1,5 +0,0 @@
-install all of flutter stuff and just do 
-
-
-
-flutter run
--- frontend/android/app/src/main/kotlin/com/example/imgrep/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.imgrep
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()

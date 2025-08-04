a53af1a00d530e7e1f874e0ad627f838a24bd2c3
samki <ssameep5@gmail.com>
2025-08-04 17:06:46 +0000

    cleaned up get caption api

--- src/routes/get_caption.py
+++ src/routes/get_caption.py
@@ -13,13 +13,17 @@ from src.utils.logger import logger
 get_caption = Blueprint('get_caption', __name__)
 
 
-@get_caption.route('/get-caption', methods=['GET'])
+@get_caption.route('/get-caption', methods=['POST'])
 def caption():
     payload = request.get_json()
     faiss_id = payload.get('faiss_id')
+    user_id = payload.get('user_id')
 
     session = get_db_session()
-    caption = session.query(ImageTable).filter(ImageTable.faiss_id == faiss_id).one()
-    
-    return jsonify({"caption": caption.description})
+    caption = session.query(ImageTable).filter(ImageTable.faiss_id == faiss_id, ImageTable.user_id == user_id).one()
+    return jsonify({
+        "status": "ok", 
+        "caption": caption.description
+        }), 200
+
 

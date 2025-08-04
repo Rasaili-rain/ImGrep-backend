0f19744dbb2918a8b8b14f2d49c217164094a077
samki <ssameep5@gmail.com>
2025-08-04 17:09:55 +0000

    get caption api cleaned

--- src/routes/get_caption.py
+++ src/routes/get_caption.py
@@ -1,17 +1,8 @@
-from datetime import date, datetime
-from flask import Blueprint, request, jsonify, Response, current_app
-from sqlalchemy import Date
-from werkzeug.datastructures import FileStorage
-from PIL import Image
-import numpy as np
-import faiss
+from flask import Blueprint, request, jsonify
 
-from src.config import Config
 from src.db import ImageTable, get_db_session
-from src.utils.logger import logger
 
 get_caption = Blueprint('get_caption', __name__)
-
 
 @get_caption.route('/get-caption', methods=['POST'])
 def caption():
@@ -21,6 +12,7 @@ def caption():
 
     session = get_db_session()
     caption = session.query(ImageTable).filter(ImageTable.faiss_id == faiss_id, ImageTable.user_id == user_id).one()
+
     return jsonify({
         "status": "ok", 
         "caption": caption.description
